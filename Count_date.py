e5c8b6e7b4087dc1e2f1d5297f51af223ecccf64
WJ-Q8808 <54425201+WJ-Q8808@users.noreply.github.com>
2020-04-26 19:42:12 +0800

    update
    #35 喵喵

--- Count_date.py
+++ Count_date.py
@@ -8,7 +8,7 @@ def opt_date(number):
         cut_date = new_time + datetime.timedelta(days=number)
         old_date_str = datetime.datetime.strftime(cut_date, "%Y-%m-%d")
         print(old_date_str)
-        print("修改信息")
+        #print("修改信息")
         return old_date_str
 
 if __name__ == '__main__':
